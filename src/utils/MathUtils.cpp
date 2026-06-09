c4f5649006f1a42dc797140830f99309b911ef33
AdarWa <adar.wasserman@gmail.com>
2026-06-09 13:44:57 +0300

    Add state matrix helper

--- src/utils/MathUtils.cpp
+++ src/utils/MathUtils.cpp
@@ -12,4 +12,22 @@ namespace kinematics {
             -v.y(), v.x(), 0.0;
         return skew;
     }
+
+    Eigen::Matrix4d poseToMatrix(const Vector6d& vec) {
+        const Eigen::Vector3d translation(vec(0), vec(1), vec(2));
+
+        const double roll  = vec(3); // Rotation around X
+        const double pitch = vec(4); // Rotation around Y
+        const double yaw   = vec(5); // Rotation around Z
+
+        Eigen::Matrix3d rotation;
+        rotation = Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ())
+                 * Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY())
+                 * Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX());
+
+        Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+        T.block<3, 3>(0, 0) = rotation;
+        T.block<3, 1>(0, 3) = translation;
+        return T;
+    }
 }
--- src/utils/MathUtils.hpp
+++ src/utils/MathUtils.hpp
@@ -5,9 +5,11 @@
 #ifndef KINEMATICS_MATHUTILS_HPP
 #define KINEMATICS_MATHUTILS_HPP
 #include <Eigen/Eigen>
+#include "../defs.hpp"
 
 namespace kinematics {
     Eigen::Matrix3d makeSkewSymmetric(const Eigen::Vector3d v);
+    Eigen::Matrix4d makeStateMatrix(const Vector6d vec);
 }
 
 #endif //KINEMATICS_MATHUTILS_HPP
