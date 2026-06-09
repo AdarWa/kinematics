2fac162515d8577831540b1d01659f445dfc6c37
AdarWa <adar.wasserman@gmail.com>
2026-06-09 14:24:25 +0300

    Fix header and cpp mismatch

--- src/utils/MathUtils.hpp
+++ src/utils/MathUtils.hpp
@@ -8,8 +8,8 @@
 #include "../defs.hpp"
 
 namespace kinematics {
-    Eigen::Matrix3d makeSkewSymmetric(const Eigen::Vector3d v);
-    Eigen::Matrix4d makeStateMatrix(const Vector6d vec);
+    Eigen::Matrix3d makeSkewSymmetric(const Eigen::Vector3d& v);
+    Eigen::Matrix4d makeStateMatrix(const Vector6d& vec);
 }
 
 #endif //KINEMATICS_MATHUTILS_HPP
