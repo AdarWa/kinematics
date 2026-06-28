1fc419ab63ae65ac1a22076004130f8bdda054d1
AdarWa <adar.wasserman@gmail.com>
2026-06-28 18:01:07 +0300

    Add ModelProvider impl

--- viewer/model/ModelProvider.cpp
+++ viewer/model/ModelProvider.cpp
@@ -4,7 +4,73 @@
 
 #include "ModelProvider.hpp"
 
+#include <fstream>
+
+#include "utils/EntityManager.h"
+
 namespace kinematics {
     namespace viewer {
+        ModelProvider::ModelProvider(filament::Engine* engine) {
+            materialProvider = filament::gltfio::createJitShaderProvider(engine);
+
+            const filament::gltfio::AssetConfiguration assetConfiguration = {
+                .engine = engine,
+                .materials = materialProvider,
+                .entities = &utils::EntityManager::get()
+            };
+            assetLoader = filament::gltfio::AssetLoader::create(assetConfiguration);
+
+            filament::gltfio::ResourceConfiguration resourceConfiguration{.engine = engine, .normalizeSkinningWeights = true};
+            resourceLoader = std::make_unique<filament::gltfio::ResourceLoader>(resourceConfiguration);
+        }
+
+        ModelProvider::~ModelProvider() {
+            for (filament::gltfio::FilamentAsset* asset : assets) {
+                assetLoader->destroyAsset(asset);
+            }
+            filament::gltfio::AssetLoader::destroy(&assetLoader);
+            materialProvider->destroyMaterials();
+            delete materialProvider;
+        }
+
+        static std::vector<uint8_t> loadFile(std::string file) {
+            std::ifstream in(file, std::ios::binary | std::ios::ate);
+            if (!in) {
+                throw std::runtime_error(std::format("Could not open file: {}", file));
+            }
+            const size_t size = in.tellg();
+            in.seekg(0, std::ios::beg);
+            std::vector<uint8_t> buffer(size);
+            in.read(reinterpret_cast<char*>(buffer.data()), size);
+            return buffer;
+        }
+
+        filament::gltfio::FilamentAsset* ModelProvider::loadAsset(std::string file) {\
+            filament::gltfio::FilamentAsset* asset = nullptr;
+
+            std::vector<uint8_t> fileBuffer = loadFile(file);
+            if (fileBuffer.empty()) {
+                throw std::runtime_error(std::format("Tried to load asset {}, it seems to be empty!", file));
+            }
+
+            asset = assetLoader->createAsset(fileBuffer.data(), fileBuffer.size());
+            if (asset == nullptr) {
+                throw std::runtime_error(std::format("Failed to parse GLB model {}", file));
+            }
+
+            resourceLoader->loadResources(asset);
+
+            assets.push_back(asset);
+            return asset;
+        }
+
+        void ModelProvider::destroyAsset(filament::gltfio::FilamentAsset* asset) {
+            std::erase(assets, asset);
+            assetLoader->destroyAsset(asset);
+        }
+
+        std::vector<filament::gltfio::FilamentAsset*> ModelProvider::getLoadedAssets() {
+            return assets;
+        }
     } // viewer
 } // kinematics
--- viewer/model/ModelProvider.hpp
+++ viewer/model/ModelProvider.hpp
@@ -5,9 +5,25 @@
 #ifndef KINEMATICS_MODELPROVIDER_HPP
 #define KINEMATICS_MODELPROVIDER_HPP
 
+#include <vector>
+#include <gltfio/AssetLoader.h>
+#include <gltfio/ResourceLoader.h>
+#include <gltfio/MaterialProvider.h>
+
 namespace kinematics {
     namespace viewer {
         class ModelProvider {
+        private:
+            filament::gltfio::MaterialProvider* materialProvider;
+            filament::gltfio::AssetLoader* assetLoader;
+            std::unique_ptr<filament::gltfio::ResourceLoader> resourceLoader;
+            std::vector<filament::gltfio::FilamentAsset*> assets;
+        public:
+            explicit ModelProvider(filament::Engine* engine);
+            ~ModelProvider();
+            filament::gltfio::FilamentAsset* loadAsset(std::string file);
+            void destroyAsset(filament::gltfio::FilamentAsset* asset);
+            std::vector<filament::gltfio::FilamentAsset*> getLoadedAssets();
         };
     } // viewer
 } // kinematics
